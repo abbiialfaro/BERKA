78418ba19309c515c4603ca7a50d08f14d0f7740
gabpc <grodriguezov@gmail.com>
2025-04-26 09:29:57 -0600

    cambios en .gitignore

---
+++ BERKA.Share/ViewModels/InspectorViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BERKA.Share.ViewModels
+{
+    public class InspectorViewModel
+    {
+        public int ID_Inspector { get; set; }
+        [Required] public string Nombre { get; set; }
+        [Required] public string Apellido { get; set; }
+        [Required] public string Correo { get; set; }
+        [Required] public string Telefono { get; set; }
+        [Required] public int Estacion { get; set; }
+        [Required] public string Contraseña { get; set; }
+        [Required] public string Estado { get; set; }
+    }
+
+}
--- BERKA.Web/obj/Debug/net8.0/BERKA.Web.AssemblyInfo.cs
+++ BERKA.Web/obj/Debug/net8.0/BERKA.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BERKA.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cff4ee433cc31dd597b16d86748f1183569c4d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78a54a4332af4fc5556738fb9820528c33fda883")]
 [assembly: System.Reflection.AssemblyProductAttribute("BERKA.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BERKA.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
